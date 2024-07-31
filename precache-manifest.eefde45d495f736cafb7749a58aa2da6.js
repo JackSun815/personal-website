8b8091f4951aaadb15220e307c93cd19ca039068
JackSun815 <zhijians@andrew.cmu.edu>
2024-07-31 16:07:29 +0800

    Updates

--- precache-manifest.eefde45d495f736cafb7749a58aa2da6.js
+++ precache-manifest.eefde45d495f736cafb7749a58aa2da6.js
@@ -1,218 +1,218 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fe39b70acd87f13f8190becb31a383ba",
-    "url": "/index.html"
+    "revision": "f5cf80a767910d417f029b3993b4b17c",
+    "url": "./index.html"
   },
   {
     "revision": "27104992abfa2dd47dac",
-    "url": "/static/css/2.08f4f2fb.chunk.css"
+    "url": "./static/css/2.08f4f2fb.chunk.css"
   },
   {
     "revision": "aaba7e59da913aeb38e6",
-    "url": "/static/css/main.ae1451e8.chunk.css"
+    "url": "./static/css/main.ae1451e8.chunk.css"
   },
   {
     "revision": "27104992abfa2dd47dac",
-    "url": "/static/js/2.2eb83fda.chunk.js"
+    "url": "./static/js/2.2eb83fda.chunk.js"
   },
   {
     "revision": "4183625f40b90d95be4480f7d388f369",
-    "url": "/static/js/2.2eb83fda.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.2eb83fda.chunk.js.LICENSE.txt"
   },
   {
     "revision": "aaba7e59da913aeb38e6",
-    "url": "/static/js/main.3284850c.chunk.js"
+    "url": "./static/js/main.3284850c.chunk.js"
   },
   {
-    "revision": "3737ef8c83feebefe91a",
-    "url": "/static/js/runtime-main.77686600.js"
+    "revision": "5901bca86445413096c0",
+    "url": "./static/js/runtime-main.7ce8a6f4.js"
   },
   {
     "revision": "2ca3aa47fe2aed34b8daeccffbbe1494",
-    "url": "/static/media/Jack-Sun-Resume.2ca3aa47.pdf"
+    "url": "./static/media/Jack-Sun-Resume.2ca3aa47.pdf"
   },
   {
     "revision": "b0f8f852a25c12de0d7bd7c1f506cc09",
-    "url": "/static/media/Shadowed Valleys-10.b0f8f852.jpg"
+    "url": "./static/media/Shadowed Valleys-10.b0f8f852.jpg"
   },
   {
     "revision": "1e91b39c4367aba2877ddf02a66a7bca",
-    "url": "/static/media/Shadowed Valleys-18.1e91b39c.jpg"
+    "url": "./static/media/Shadowed Valleys-18.1e91b39c.jpg"
   },
   {
     "revision": "5a62db60f78f764e2c021e9776e17388",
-    "url": "/static/media/Shadowed Valleys-19.5a62db60.jpg"
+    "url": "./static/media/Shadowed Valleys-19.5a62db60.jpg"
   },
   {
     "revision": "8d65e7a26e6ce03814a1914dee2fa4c7",
-    "url": "/static/media/Shadowed Valleys-20.8d65e7a2.jpg"
+    "url": "./static/media/Shadowed Valleys-20.8d65e7a2.jpg"
   },
   {
     "revision": "32e3eb086e280b825cd33ada3d17a933",
-    "url": "/static/media/Shadowed Valleys-21.32e3eb08.jpg"
+    "url": "./static/media/Shadowed Valleys-21.32e3eb08.jpg"
   },
   {
     "revision": "ac1b65d7fd3f5974ad0a0cf226ad4707",
-    "url": "/static/media/Shadowed Valleys-22.ac1b65d7.jpg"
+    "url": "./static/media/Shadowed Valleys-22.ac1b65d7.jpg"
   },
   {
     "revision": "101236b09c00e0f4c4093d15a8e9c4a9",
-    "url": "/static/media/Shadowed Valleys-26.101236b0.jpg"
+    "url": "./static/media/Shadowed Valleys-26.101236b0.jpg"
   },
   {
     "revision": "191adbb71fd0fd031abb88876c9458eb",
-    "url": "/static/media/Shadowed Valleys-28.191adbb7.jpg"
+    "url": "./static/media/Shadowed Valleys-28.191adbb7.jpg"
   },
   {
     "revision": "4ad2157cfd7b4117dd768530e522d007",
-    "url": "/static/media/Shadowed Valleys-8.4ad2157c.jpg"
+    "url": "./static/media/Shadowed Valleys-8.4ad2157c.jpg"
   },
   {
     "revision": "4ab94507369c037fafaad663d695b4d1",
-    "url": "/static/media/Shadowed Valleys-9.4ab94507.jpg"
+    "url": "./static/media/Shadowed Valleys-9.4ab94507.jpg"
   },
   {
     "revision": "7f8be92e5e5df59b2a2bc8b8ad566779",
-    "url": "/static/media/Steel City-2.7f8be92e.jpg"
+    "url": "./static/media/Steel City-2.7f8be92e.jpg"
   },
   {
     "revision": "3310ebc6c1d12f22f4a0e2beab8e1900",
-    "url": "/static/media/Steel City-3.3310ebc6.jpg"
+    "url": "./static/media/Steel City-3.3310ebc6.jpg"
   },
   {
     "revision": "fb9b2a95e8987deafc78b341087d79a4",
-    "url": "/static/media/Steel City-4.fb9b2a95.jpg"
+    "url": "./static/media/Steel City-4.fb9b2a95.jpg"
   },
   {
     "revision": "2cb53922a20d066083957ccb705bb839",
-    "url": "/static/media/Steel City-5.2cb53922.jpg"
+    "url": "./static/media/Steel City-5.2cb53922.jpg"
   },
   {
     "revision": "582abef721a11da8e71b6abd9aee81da",
-    "url": "/static/media/Steel City-6.582abef7.jpg"
+    "url": "./static/media/Steel City-6.582abef7.jpg"
   },
   {
     "revision": "cccfcd29ddb04ea692f8bcc2f788216e",
-    "url": "/static/media/Steel City.cccfcd29.jpg"
+    "url": "./static/media/Steel City.cccfcd29.jpg"
   },
   {
     "revision": "aa3c424a2cb9b14ab11c0d1b6a393c71",
-    "url": "/static/media/about-photo.aa3c424a.png"
+    "url": "./static/media/about-photo.aa3c424a.png"
   },
   {
     "revision": "b2e6774fd8752d966e816bd5b5d6f2c1",
-    "url": "/static/media/beatbox-screenshot1.b2e6774f.png"
+    "url": "./static/media/beatbox-screenshot1.b2e6774f.png"
   },
   {
     "revision": "36d8776020f60c237266a8668c92116b",
-    "url": "/static/media/beatbox-screenshot2.36d87760.png"
+    "url": "./static/media/beatbox-screenshot2.36d87760.png"
   },
   {
     "revision": "630b8590038ec08d242f11e4e0d28692",
-    "url": "/static/media/beatbox.630b8590.jpg"
+    "url": "./static/media/beatbox.630b8590.jpg"
   },
   {
     "revision": "e1c3bbf7d25380eb76e8da58016f241c",
-    "url": "/static/media/chalfant-screenshot1.e1c3bbf7.png"
+    "url": "./static/media/chalfant-screenshot1.e1c3bbf7.png"
   },
   {
     "revision": "f2a5d6b5cba169105567583098099b0b",
-    "url": "/static/media/chalfant-screenshot2.f2a5d6b5.png"
+    "url": "./static/media/chalfant-screenshot2.f2a5d6b5.png"
   },
   {
     "revision": "7ebdfa69fbaf46bda5b0b8273526df71",
-    "url": "/static/media/chalfant-screenshot3.7ebdfa69.png"
+    "url": "./static/media/chalfant-screenshot3.7ebdfa69.png"
   },
   {
     "revision": "f7e461c3a1d7d86ecde403f15d9da716",
-    "url": "/static/media/gcpd-erd.f7e461c3.png"
+    "url": "./static/media/gcpd-erd.f7e461c3.png"
   },
   {
     "revision": "50dd0065296752daadb128f08ba57888",
-    "url": "/static/media/gcpd-screenshot1.50dd0065.png"
+    "url": "./static/media/gcpd-screenshot1.50dd0065.png"
   },
   {
     "revision": "05dd6f62872ae52676cd53200e03d1f6",
-    "url": "/static/media/gcpd-screenshot2.05dd6f62.png"
+    "url": "./static/media/gcpd-screenshot2.05dd6f62.png"
   },
   {
     "revision": "ca687b66e38a5482bd899976604e7b7f",
-    "url": "/static/media/gcpd-screenshot3.ca687b66.png"
+    "url": "./static/media/gcpd-screenshot3.ca687b66.png"
   },
   {
     "revision": "01641743fb58d6433302e7c8fea80b04",
-    "url": "/static/media/gcpd-screenshot4.01641743.png"
+    "url": "./static/media/gcpd-screenshot4.01641743.png"
   },
   {
     "revision": "fef5e49446f072febd248442757df8b9",
-    "url": "/static/media/gcpd-screenshot5.fef5e494.png"
+    "url": "./static/media/gcpd-screenshot5.fef5e494.png"
   },
   {
     "revision": "315f64d200d5196d7300170baaea1281",
-    "url": "/static/media/gcpd.315f64d2.png"
+    "url": "./static/media/gcpd.315f64d2.png"
   },
   {
     "revision": "f6f32c6911e7e02c5f572451e3cff424",
-    "url": "/static/media/hero-right.f6f32c69.png"
+    "url": "./static/media/hero-right.f6f32c69.png"
   },
   {
     "revision": "679c1d5a84c5abe32b45992bd2253579",
-    "url": "/static/media/in-progress.679c1d5a.png"
+    "url": "./static/media/in-progress.679c1d5a.png"
   },
   {
     "revision": "11dd61a7f931b6f7a3bb39cb521c3025",
-    "url": "/static/media/news.11dd61a7.png"
+    "url": "./static/media/news.11dd61a7.png"
   },
   {
     "revision": "cf6edbad924f8fbf87cfc12399fe76d4",
-    "url": "/static/media/project-chalfant.cf6edbad.png"
+    "url": "./static/media/project-chalfant.cf6edbad.png"
   },
   {
     "revision": "ed11c87ef8dad7fee2ec67e6c05cfa34",
-    "url": "/static/media/project-threekingdom.ed11c87e.png"
+    "url": "./static/media/project-threekingdom.ed11c87e.png"
   },
   {
     "revision": "61ce2c60293a4b50b2e8d38369508866",
-    "url": "/static/media/project-tutorial.61ce2c60.png"
+    "url": "./static/media/project-tutorial.61ce2c60.png"
   },
   {
     "revision": "756eac27444d508c17e596253990eb1c",
-    "url": "/static/media/project-visualization.756eac27.png"
+    "url": "./static/media/project-visualization.756eac27.png"
   },
   {
     "revision": "46a15dadf605207089cea02d06c3c7a7",
-    "url": "/static/media/threekingdom-screenshot1.46a15dad.png"
+    "url": "./static/media/threekingdom-screenshot1.46a15dad.png"
   },
   {
     "revision": "5671574e365a14c9db6db0bb6541afda",
-    "url": "/static/media/threekingdom-screenshot2.5671574e.png"
+    "url": "./static/media/threekingdom-screenshot2.5671574e.png"
   },
   {
     "revision": "7f681a5b53c1451e2c661187e9208342",
-    "url": "/static/media/threekingdom-screenshot3.7f681a5b.png"
+    "url": "./static/media/threekingdom-screenshot3.7f681a5b.png"
   },
   {
     "revision": "45917d5020da6cab2030a92f760727c5",
-    "url": "/static/media/tutorial-screenshot1.45917d50.png"
+    "url": "./static/media/tutorial-screenshot1.45917d50.png"
   },
   {
     "revision": "7242217d12a7453012017614d183c541",
-    "url": "/static/media/tutorial-screenshot2.7242217d.png"
+    "url": "./static/media/tutorial-screenshot2.7242217d.png"
   },
   {
     "revision": "bb6fa71eaa16583e172d08fc7db51a0c",
-    "url": "/static/media/tutorial-screenshot3.bb6fa71e.png"
+    "url": "./static/media/tutorial-screenshot3.bb6fa71e.png"
   },
   {
     "revision": "756eac27444d508c17e596253990eb1c",
-    "url": "/static/media/wals-screenshot1.756eac27.png"
+    "url": "./static/media/wals-screenshot1.756eac27.png"
   },
   {
     "revision": "2ba91b8d8f88fc5a4b0f880a7e54d81b",
-    "url": "/static/media/wals-screenshot2.2ba91b8d.png"
+    "url": "./static/media/wals-screenshot2.2ba91b8d.png"
   },
   {
     "revision": "b94a4898e9c3f67962a52de0f4cd03cb",
-    "url": "/static/media/wals-screenshot3.b94a4898.png"
+    "url": "./static/media/wals-screenshot3.b94a4898.png"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f7adad3c3f3b4e5bac43606255395965.js"
+  "./precache-manifest.eefde45d495f736cafb7749a58aa2da6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
